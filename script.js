2c048b8df1964156f11b24aa0228be66b3e21ef5
sagarpani <sagarpani22@gmail.com>
2025-12-01 22:46:46 +0530

    localStorage added and reset button added

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-
+//Declaration
 let result, computerMove;
-let score = { win: 0, loss: 0, tie: 0 };
+let score = {
+    win: 0,
+    loss: 0,
+    tie: 0
+};
 
+//Computer Move
 function pickComputerMove() {
 
     const randomNumber = Math.random();
 
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        return 'rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        return 'paper';
-    }
-    else {
-        return 'scissors';
-    }
+    if (randomNumber < 1 / 3) return 'rock';
+    if (randomNumber < 2 / 3) return 'paper';
+    return 'scissors';
 }
-document.getElementById('rock').addEventListener('click', function(){
-    play('rock');
-});
-document.getElementById('paper').addEventListener('click', function(){
-    play('paper')
-});
-document.getElementById('scissors').addEventListener('click', function(){
-    play('scissors')
-});
 
-// document.getElementById('reset').addEventListener(
-//     'click', function(){
-//         score.win = 0;
-//         score.loss = 0;
-//         score.tie = 0;
-//     }
-// );
+//Add event listeners
+const playerMove = (id) => {
+    document.getElementById(id).addEventListener('click', () => play(id))
+};
 
+playerMove('rock');
+playerMove('paper');
+playerMove('scissors');
+
+//Reset Section
+playerMove('reset');
+
+//Human Move
 function playGames(humanMove) {
-
     computerMove = pickComputerMove();
     if (computerMove === humanMove) result = 'tie';
     else if (
@@ -45,14 +40,41 @@ function playGames(humanMove) {
 }
 
 function play(choice) {
-    ({ result, computerMove } = playGames(choice));
-    score[result]++;
+    //Display the box
     const box = document.getElementById('result-box');
+
+    //Loading existing score or initialize the score
+    let stored = JSON.parse(localStorage.getItem('score')) || {
+        win: 0,
+        loss: 0,
+        tie: 0
+    };
+
+    //Reset the game
+    if (choice === 'reset') {
+        stored = { win: 0, loss: 0, tie: 0 };
+        localStorage.setItem("score", JSON.stringify(stored));
+
+        box.style.display = 'block';
+        box.innerHTML = `
+                    The score has been reset.<br>
+                    Wins: ${stored.win} || Losses: ${stored.loss} || Ties: ${stored.tie}
+                `;
+        return;
+    }
+    //Play the game
+
+    ({ result, computerMove } = playGames(choice));
+
+    //changes have been made
+    stored[result]++;
+    localStorage.setItem('score', JSON.stringify(stored));
+
     box.style.display = 'block';
     box.innerHTML = `
                 Your choice: <strong>${choice}</strong><br>
                 Computer choice: <strong>${computerMove}</strong><br>
                 Result: <strong>${result.toUpperCase()}</strong><br>
-                Wins: ${score.win} || Losses: ${score.loss} || Ties: ${score.tie}
+                Wins: ${stored.win} || Losses: ${stored.loss} || Ties: ${stored.tie}
             `;
 }
